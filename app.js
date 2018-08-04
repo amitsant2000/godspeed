b22cf3cbe0ca4bcfa81e975199be059bd8944991
Amit Sant <amitsant.2000@gmail.com>
2018-08-04 04:08:51 -0700

    multiplayer implemented

--- app.js
+++ app.js
@@ -1,46 +1,29 @@
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
-var exphbs = require('express-handlebars');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-io.on('connection',function(socket){
-  socket.on('message',function(msg){
-    if(socket.username){
-      var z = socket.username;
-      console.log(z)
-      io.emit('serverMsg',{msg:msg,user:z})
-    }else{
-      socket.emit('alertMsg',"Log in first boyo")
-    }
-  })
-  socket.on('login',function(diseroni){
-    if(diseroni.length!==0){
-      socket.username = diseroni
-      socket.emit('loginResp','You have joined the room')
-      socket.broadcast.emit('otherDude',`${diseroni} has joined the room`)
-    }else{
-      socket.emit('alertMsg','Usename must not be empty')
-    }
-  })
-})
-// Set View Engine
-app.engine('html', exphbs({
-  extname: 'html'
+var exphbs = require('express-handlebars');
+var rooms = []
+app.engine('hbs', exphbs({
+  extname: 'hbs'
 }));
-app.set('view engine', 'html');
-
-// Static assets
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Logging
-app.use(morgan('combined'));
-
-app.get('/', function(req, res) {
-  res.render('index');
+app.set('view engine', 'hbs');
+io.on('connection', function(socket) {
+  socket.on('sendme',function(mover){
+    if(!socket.user){
+      socket.user = mover.username;
+    }
+    socket.broadcast.emit('enemysent',mover)
+  })
+  socket.on('sendbullet',function(mover){
+    socket.broadcast.emit('enemybullet',mover)
+  })
+  socket.on('hit',function(a){
+    io.emit('enemysent',a)
+  })
 });
-
 var port = process.env.PORT || 3000;
 server.listen(port, function(){
   console.log('Express started. Listening on %s', port);
